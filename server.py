097d220db8d47b6622b52ae28d4bb100fe5ed561
andres9jaen <156694184+andres9jaen@users.noreply.github.com>
2025-08-04 19:58:16 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,17 +5,21 @@ import os
 
 PORT = int(os.environ.get("PORT", 8000))
 
-class Handler(http.server.SimpleHTTPRequestHandler):
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/config":
+        if self.path == "/":
+            self.path = "/templates/index.html"
+        elif self.path == "/admin":
+            self.path = "/templates/admin.html"
+        elif self.path == "/config":
             with open("config.json", "r", encoding="utf-8") as f:
                 config = f.read().encode("utf-8")
             self.send_response(200)
             self.send_header("Content-Type", "application/json; charset=utf-8")
             self.end_headers()
             self.wfile.write(config)
-        else:
-            super().do_GET()
+            return
+        return super().do_GET()
 
     def do_POST(self):
         if self.path == "/update_config":
@@ -40,6 +44,6 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(b'{"status": "ok"}')
 
 os.chdir(os.path.dirname(__file__))
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"🎲 Servidor corriendo en el puerto {PORT}")
     httpd.serve_forever()
